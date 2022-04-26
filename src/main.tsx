4f56e32e3c6f7542de8c81e9c5c1920445695f37
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-25 21:22:29 -0500

    chore(package): add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement: HTMLElement | null = document.getElementById('root')
+
+if (!rootElement) throw new Error('Root element not found')
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
