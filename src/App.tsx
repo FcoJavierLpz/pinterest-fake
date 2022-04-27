9ad26befebbbb578f8af940da51ecf323051a524
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-27 13:53:14 -0500

    refactor(app): implementing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './pages/SignIn'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
 function App() {
-  return <></>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
