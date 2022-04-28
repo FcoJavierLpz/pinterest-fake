3a010276b53f087f41b1df15d6a34fd6fef19501
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-27 23:06:21 -0500

    refactor(signin): allow a user to login with email and password

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Alert from '../components/Alert'
+import { useAuth } from '../context/authContext'
 
-const Login = () => {
-  return <div>Login</div>
+const SignIn = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [error, setError] = useState<string | undefined>('')
+
+  const { signIn, loginWithGoogle } = useAuth()
+  const navigate = useNavigate()
+
+  const handleChange = ({
+    target: { name, value }
+  }: React.ChangeEvent<HTMLInputElement>) => setUser({ ...user, [name]: value })
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError('')
+    try {
+      await signIn(user.email, user.password)
+      navigate('/')
+    } catch (err: any) {
+      setError(err?.message)
+    }
+  }
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await loginWithGoogle()
+      navigate('/')
+    } catch (err: any) {
+      setError(err?.message)
+    }
+  }
+  return (
+    <div>
+      {error && <Alert message={error} />}
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="youremail@company.ltd"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="********"
+          onChange={handleChange}
+        />
+        <button>SignIn</button>
+      </form>
+      <button onClick={handleGoogleSignIn}>Google Login</button>
+    </div>
+  )
 }
 
-export default Login
+export default SignIn
