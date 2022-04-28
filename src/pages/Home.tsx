8ed80b6d681d8510411f7e716635624482013abb
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-27 23:15:04 -0500

    refactor(home): displays session data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useAuth } from '../context/authContext'
 
 const Home = () => {
-  return <div>Home</div>
+  const { user, logout } = useAuth()
+
+  const handleLogout = () => {
+    logout()
+  }
+
+  return (
+    <div>
+      <h1>Welcome {user.displayName || user.email}</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
 }
 
 export default Home
