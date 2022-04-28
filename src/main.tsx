3c5259ca914abb1827c9fce011a5a11c3fc8d077
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-27 22:46:52 -0500

    refactor(main): improve bootstrapping

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/index.css'
 
-const rootElement: HTMLElement | null = document.getElementById('root')
-
-if (!rootElement) throw new Error('Root element not found')
-
-const root = ReactDOM.createRoot(rootElement)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLDivElement
+)
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 )
