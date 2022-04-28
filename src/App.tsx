b84a5ae0345de28d8957de8e9d426bcb89585438
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-27 22:50:25 -0500

    refactor(app): add router guard with auth context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/SignIn'
+import { Route, Routes } from 'react-router-dom'
+import AuthContextProvider from './context/authContextProvider'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import ProtectedRoute from './pages/ProtectedRoute'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <Router>
+    <AuthContextProvider>
       <Routes>
-        <Route path="/signin" element={<Login />} />
-        <Route path="/signup" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </AuthContextProvider>
   )
 }
 
