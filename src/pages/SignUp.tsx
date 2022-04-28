a03750f161c79b52798a36710145d796549d844e
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-28 10:54:11 -0500

    refactor(signup): implement loggin service

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Alert from '../components/Alert'
 import useAuth from '../hooks/useAuth'
+import { getErrorMessage, reportError } from '../services/logger'
 
 const SignUp = () => {
   const [user, setUser] = useState({
     email: '',
     password: ''
   })
-
-  const [error, setError] = useState<string | undefined>('')
 
   const { signUp } = useAuth()
   const navigate = useNavigate()
@@ -20,20 +18,17 @@ const SignUp = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    setError('')
 
     try {
       await signUp?.(user.email, user.password)
       navigate('/')
-    } catch (err: any) {
-      setError(err?.message)
+    } catch (err) {
+      reportError({ message: getErrorMessage(err) })
     }
   }
 
   return (
     <div>
-      {error && <Alert message={error} />}
-
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input
