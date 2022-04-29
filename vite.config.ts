476e51c1c1ff6050c11dbf9830a179793d31d349
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-29 18:45:45 -0500

    chore(config): add reverse proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'https://api.imgur.com/3/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
