09460a5057fe5284c7b9ef546c5f8cf78c0a0a38
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-29 19:34:43 -0500

    refactor(requireauth): cbheck the user loading

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -3,8 +3,10 @@ import useAuth from '../hooks/useAuth'
 import { Navigate, useLocation } from 'react-router-dom'
 
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
-  const { user } = useAuth()
+  const { user, loading } = useAuth()
   const location = useLocation()
+
+  if (loading) return <h1>Loading</h1>
 
   if (!user) return <Navigate to="/login" state={{ from: location }} replace />
 
