3051314dbf2ab39f2013514a42a6466bc6526218
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-29 20:20:28 -0500

    refactor(config): improve firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,25 +1,8 @@
 import { initializeApp } from 'firebase/app'
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut
-} from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import config from './config'
 
 const FirebaseApp = initializeApp(config.firebaseConfig)
-
-export const Providers = {
-  Google: GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signInWithSocialMedia: signInWithPopup,
-  signOut
-}
 
 export const auth = getAuth(FirebaseApp)
 
