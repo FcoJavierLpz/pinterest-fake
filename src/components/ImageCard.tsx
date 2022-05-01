6afc45610624f888ec91f85bfab492cd41b41ba6
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-01 17:52:06 -0500

    refactor(image-card): apply lazy loading

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,25 +1,36 @@
 import { Image } from '../interfaces/IGallery'
+import { useInView } from 'react-intersection-observer'
 
 type Props = {
   img: Image
 }
 
 const ImageCard = ({ img }: Props) => {
-  if (img?.type.startsWith('image')) {
-    return (
-      <figure className="py-4 [break-inside:avoid]">
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: '200px 0px'
+  })
+
+  return (
+    <figure
+      className="py-4 [break-inside:avoid]"
+      ref={ref}
+      data-inview={inView}
+    >
+      {inView ? (
         <img
           className="rounded-md"
           src={img.link}
           alt={img.description || undefined}
+          loading="lazy"
         />
-        <figcaption className="mt-2">{img?.title}</figcaption>
-        <p className="text-sm text-gray-500 line-clamp-2">{img.description}</p>
-      </figure>
-    )
-  }
-
-  return null
+      ) : (
+        <div>Load Image...</div>
+      )}
+      <figcaption className="mt-2">{img?.title}</figcaption>
+      <p className="text-sm text-gray-500 line-clamp-2">{img.description}</p>
+    </figure>
+  )
 }
 
 export default ImageCard
