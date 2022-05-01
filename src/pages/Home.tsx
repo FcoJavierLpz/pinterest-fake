ba69a1c0d1d7afb4c1c88cf266f380c7eb25429f
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-04-30 20:57:38 -0500

    refactor(pages): add scaffolding for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,25 @@
+import Gallery from '../components/Gallery'
+import Loading from '../components/icons/Loading'
+import NavBar from '../components/NavBar'
 import useAuth from '../hooks/useAuth'
 
 const Home = () => {
-  const { user, logout } = useAuth()
-
-  const handleLogout = () => {
-    logout()
-  }
+  const { user } = useAuth()
 
   return (
-    <div>
-      <h1>Welcome {user.displayName || user.email}</h1>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="w-full min-h-screen font-sans text-gray-900">
+      <NavBar />
+      <main className="py-14 px-4 mx-auto max-w-7xl">
+        <div className="flex items-center">
+          <h1 className="text-2xl font-medium">
+            Bienvenido {user?.displayName || user?.email}
+          </h1>
+        </div>
+        <Gallery />
+        <div className="flex justify-center items-center py-12">
+          <Loading className="w-8 h-8 text-indigo-600 animate-spin" />
+        </div>
+      </main>
     </div>
   )
 }
