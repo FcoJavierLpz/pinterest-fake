e6ac0580ec85ca0f64d3a184915f3e0e835caad3
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 11:49:44 -0500

    refactor(pages): remove username into home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,11 @@
 import Gallery from '../components/Gallery'
 import NavBar from '../components/NavBar'
-import useAuth from '../hooks/useAuth'
 
 const Home = () => {
-  const { user } = useAuth()
-
   return (
     <div className="w-full min-h-screen font-sans text-gray-900">
       <NavBar />
       <main className="py-14 px-4 mx-auto max-w-7xl">
-        <div className="flex items-center">
-          <h1 className="text-2xl font-medium">
-            Bienvenido {user?.displayName || user?.email}
-          </h1>
-        </div>
         <Gallery />
       </main>
     </div>
