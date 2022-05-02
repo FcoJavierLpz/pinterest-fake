91f3ada4e293ed057740c9d4be74337a8e76da1c
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 12:21:03 -0500

    refactor(components): set loading for required auth page

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,12 +1,18 @@
 import useAuth from '../hooks/useAuth'
 
 import { Navigate, useLocation } from 'react-router-dom'
+import Loading from './icons/Loading'
 
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const { user, loading } = useAuth()
   const location = useLocation()
 
-  if (loading) return <h1>Loading</h1>
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loading className="w-12 h-12 text-indigo-600 animate-spin" />
+      </div>
+    )
 
   if (!user) return <Navigate to="/login" state={{ from: location }} replace />
 
