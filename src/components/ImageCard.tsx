f880b34a39fc8856f06ac22ec0d634139966963d
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 13:33:02 -0500

    refactor(components): improve image loading

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,11 +1,14 @@
 import { Image } from '../interfaces/IGallery'
 import { useInView } from 'react-intersection-observer'
+import useImageOnLoad from '../hooks/useImageOnLoad'
 
 type Props = {
   img: Image
 }
 
 const ImageCard = ({ img }: Props) => {
+  const { handleImageOnLoad, css } = useImageOnLoad()
+
   const { ref, inView } = useInView({
     triggerOnce: true,
     rootMargin: '200px 0px'
@@ -13,17 +16,26 @@ const ImageCard = ({ img }: Props) => {
 
   return (
     <figure
-      className="py-4 [break-inside:avoid]"
+      className="py-4 [break-inside:avoid] relative"
       ref={ref}
       data-inview={inView}
     >
       {inView ? (
-        <img
-          className="rounded-md"
-          src={img.link}
-          alt={img.description || undefined}
-          loading="lazy"
-        />
+        <>
+          <img
+            className="rounded-md absolute"
+            src={img.link}
+            alt={img.description || undefined}
+            style={{ ...css.thumbnail }}
+          />
+          <img
+            className="rounded-md"
+            src={img.link}
+            alt={img.description || undefined}
+            onLoad={handleImageOnLoad}
+            style={{ ...css.fullSize }}
+          />
+        </>
       ) : (
         <div>Load Image...</div>
       )}
