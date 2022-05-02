c9281c252c75ef308e86375f69e9698fa706432b
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 11:53:24 -0500

    refactor(components): use custom hook for gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 
 import Loading from '../components/icons/Loading'
-import { Image } from '../interfaces/IGallery'
-import { getGallery } from '../services/gallery'
+import useGallery from '../hooks/useGallery'
 import ImageCard from './ImageCard'
+import SearchBox from './SearchBox'
 
 const Gallery = () => {
-  const [loading, setLoading] = useState(true)
-  const [images, setImages] = useState<Image[]>([])
   const [pageNum, setPageNum] = useState(0)
+  const [searchQuery, setSearchQuery] = useState('')
+  const { fetchGallery, fetchGalleryByTags, images, loading } = useGallery()
+
+  const MIN_LENGTH_SEARCH = 3
 
   const { ref, inView } = useInView({
     triggerOnce: true
@@ -21,47 +23,40 @@ const Gallery = () => {
     }
   }, [inView])
 
-  const getImagesFromSubReddit = subReddit =>
-    subReddit
-      ?.filter(g => {
-        return (
-          g?.images?.length && g.images.every(i => i.type.startsWith('image'))
-        )
-      })
-      .map(el => el.images)
-      .flat()
-
-  const fetchGallery = async () => {
-    try {
-      setLoading(true)
-      const {
-        data: { data: subRedditFromApi }
-      } = await getGallery('top', 'day', pageNum)
-
-      setImages(imgs => [...imgs, ...getImagesFromSubReddit(subRedditFromApi)])
-    } finally {
-      setLoading(true)
-    }
-  }
+  useEffect(() => {
+    fetchGallery(pageNum)
+  }, [pageNum])
 
   useEffect(() => {
-    fetchGallery()
-  }, [pageNum])
+    if (searchQuery.length > MIN_LENGTH_SEARCH) {
+      fetchGalleryByTags(searchQuery)
+    } else {
+      setPageNum(0)
+      fetchGallery(pageNum)
+    }
+  }, [searchQuery])
 
   return (
     <>
+      <div className="flex items-center">
+        <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      </div>
       <div className="gap-6 pt-2 columns-3xs">
-        {images?.map((image, index) => {
-          if (images[images.length - 1].id !== image.id) {
-            return <ImageCard key={`${index}${image.id}`} img={image} />
-          }
+        {images?.length ? (
+          images.map((image, index) => {
+            if (images[images.length - 1].id !== image.id) {
+              return <ImageCard key={`${index}${image.id}`} img={image} />
+            }
 
-          return (
-            <div key={`${index}${image.id}`} ref={ref} data-inview={inView}>
-              <ImageCard img={image} />
-            </div>
-          )
-        })}
+            return (
+              <div key={`${index}${image.id}`} ref={ref} data-inview={inView}>
+                <ImageCard img={image} />
+              </div>
+            )
+          })
+        ) : (
+          <div className="mx-5">Sin Resultados</div>
+        )}
       </div>
       {loading ? (
         <div className="flex justify-center items-center py-12">
