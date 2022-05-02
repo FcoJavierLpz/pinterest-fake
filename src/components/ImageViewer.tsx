4fcf8e931cea04adf9793b7459f5dbe5d1f5e0f9
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 15:21:31 -0500

    feat(components): add image viewer component

---
+++ src/components/ImageViewer.tsx
@@ -0,0 +1,134 @@
+import React, { CSSProperties, useCallback, useEffect, useState } from 'react'
+import styles from '../styles/image-viewer.module.css'
+
+interface IProps {
+  src: string[]
+  currentIndex?: number
+  backgroundStyle?: CSSProperties
+  disableScroll?: boolean
+  closeOnClickOutside?: boolean
+  onClose?: () => void
+  closeComponent?: JSX.Element
+  leftArrowComponent?: JSX.Element
+  rightArrowComponent?: JSX.Element
+}
+
+const ImageViewer = (props: IProps) => {
+  const [currentIndex, setCurrentIndex] = useState(props.currentIndex ?? 0)
+
+  const changeImage = useCallback(
+    (delta: number) => {
+      let nextIndex = (currentIndex + delta) % props.src.length
+      if (nextIndex < 0) nextIndex = props.src.length - 1
+      setCurrentIndex(nextIndex)
+    },
+    [currentIndex]
+  )
+
+  const handleClick = useCallback(
+    (event: any) => {
+      if (!event.target || !props.closeOnClickOutside) {
+        return
+      }
+
+      const checkId = event.target.id === 'ReactSimpleImageViewer'
+      const checkClass = event.target.classList.contains('viewer__slide')
+
+      if (checkId || checkClass) {
+        event.stopPropagation()
+        props.onClose?.()
+      }
+    },
+    [props.onClose]
+  )
+
+  const handleKeyDown = useCallback(
+    (event: any) => {
+      if (event.key === 'Escape') {
+        props.onClose?.()
+      }
+
+      if (['ArrowLeft', 'h'].includes(event.key)) {
+        changeImage(-1)
+      }
+
+      if (['ArrowRight', 'l'].includes(event.key)) {
+        changeImage(1)
+      }
+    },
+    [props.onClose, changeImage]
+  )
+
+  const handleWheel = useCallback(
+    (event: any) => {
+      if (event.wheelDeltaY > 0) {
+        changeImage(-1)
+      } else {
+        changeImage(1)
+      }
+    },
+    [changeImage]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+
+    if (!props.disableScroll) {
+      document.addEventListener('wheel', handleWheel)
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+
+      if (!props.disableScroll) {
+        document.removeEventListener('wheel', handleWheel)
+      }
+    }
+  }, [handleKeyDown, handleWheel])
+
+  return (
+    <div
+      id="ReactSimpleImageViewer"
+      className={`${styles.wrapper} viewer__modal`}
+      onKeyDown={handleKeyDown}
+      onClick={handleClick}
+      style={props.backgroundStyle}
+    >
+      <span
+        className={`${styles.close} viewer__close`}
+        onClick={() => props.onClose?.()}
+      >
+        {props.closeComponent || '×'}
+      </span>
+
+      {props.src.length > 1 && (
+        <span
+          className={`${styles.navigation} ${styles.prev} viewer__previous`}
+          onClick={() => changeImage(-1)}
+        >
+          {props.leftArrowComponent || '❮'}
+        </span>
+      )}
+
+      {props.src.length > 1 && (
+        <span
+          className={`${styles.navigation} ${styles.next} viewer__next`}
+          onClick={() => changeImage(1)}
+        >
+          {props.rightArrowComponent || '❯'}
+        </span>
+      )}
+
+      <div
+        className={`${styles.content} viewer__modal-content`}
+        onClick={handleClick}
+      >
+        <div className={`${styles.slide} viewer__slide`}>
+          <img className={styles.image} src={props.src[currentIndex]} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ImageViewer
