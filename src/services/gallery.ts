2314276f5ac833aecf7ceabb6ab4f7ba38d73ea6
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-02 11:46:31 -0500

    refactor(services): add get gallery by tags

--- src/services/gallery.ts
+++ src/services/gallery.ts
@@ -4,8 +4,17 @@ import { SORT, WINDOW, PAGE } from '../constants/gallery/pathParameters'
 
 export const getGallery = (
   sort: string = SORT.TOP,
-  window: string = WINDOW.DAY,
+  window: string = WINDOW.WEEK,
   page: number = PAGE.ZERO
 ): Promise<AxiosResponse> => {
   return http.get(`/api/gallery/${sort}/${window}/${page}`)
+}
+
+export const getGalleryByTags = (
+  tagName: string,
+  sort: string = SORT.TOP,
+  window: string = WINDOW.WEEK,
+  page: number = PAGE.ZERO
+): Promise<AxiosResponse> => {
+  return http.get(`/api/gallery/t/${tagName}/${sort}/${window}/${page}`)
 }
