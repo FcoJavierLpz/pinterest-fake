fa07d85bc709bbe65573d869ada1b3229962b9ee
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-03 16:36:39 -0500

    refactor(components): remove hidden for responsive mobile

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,7 +3,7 @@ const SearchBox = ({ searchQuery, setSearchQuery }) => {
     <input
       type="text"
       name="query"
-      className="hidden sm:inline-block flex-1 py-1.5 px-4 mx-4 text-gray-700 bg-gray-100 rounded-full border border-gray-100 transition focus:outline-none focus:bg-white focus:border-gray-700"
+      className="sm:inline-block flex-1 py-1.5 px-4 mx-4 text-gray-700 bg-gray-100 rounded-full border border-gray-100 transition focus:outline-none focus:bg-white focus:border-gray-700"
       placeholder="Search"
       value={searchQuery}
       onChange={e => setSearchQuery(e.target.value)}
